c1786ae3e57f998a84c848bf4fb0b9689302d685
resh <reshmansiva96@gmail.com>
2021-12-20 16:22:57 +0000

    initalising

--- src/main/java/employee_csv_proj/model/Employee.java
+++ src/main/java/employee_csv_proj/model/Employee.java
@@ -1,5 +1,8 @@
 package employee_csv_proj.model;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Employee {
     // data we gather about employee from employee CSV file
     private int empId;
@@ -9,8 +12,92 @@ public class Employee {
     private String lastName;
     private String gender;
     private String email;
-    private String dob;
-    private String dateOfJoining;
+    private LocalDate dob;
+    private LocalDate dateOfJoining;
     private int salary;
 
+    public Employee(String[] employeeData) {
+        this.setEmpId(Integer.parseInt(employeeData[0]));
+    }
+
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getNamePrefix() {
+        return namePrefix;
+    }
+
+    public void setNamePrefix(String namePrefix) {
+        this.namePrefix = namePrefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public void setMiddleInitial(String middleInitial) {
+        this.middleInitial = middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = LocalDate.parse(dob, DateTimeFormatter.ofPattern("M[M]/d[d]/yyyy"));
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(String dateOfJoining) {
+        this.dateOfJoining = LocalDate.parse(dateOfJoining, DateTimeFormatter.ofPattern("M[M]/d[d]/yyyy"));
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
