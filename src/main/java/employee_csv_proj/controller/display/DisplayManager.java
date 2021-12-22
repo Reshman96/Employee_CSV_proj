452f995fbcdf448ea4d66b5ef943fc69b592361f
resh <reshmansiva96@gmail.com>
2021-12-22 12:05:19 +0000

    creating DisplayManager

--- src/main/java/employee_csv_proj/controller/display/DisplayManager.java
+++ src/main/java/employee_csv_proj/controller/display/DisplayManager.java
@@ -5,8 +5,6 @@ public class DisplayManager {
     public static void requestCommand() {
         System.out.println("Please select what you would like to do: ");
         System.out.println("1) Add a new employee to the employee database \n" +
-                "2) Retrieve information about existing employees \n" +
-                "3) Update an existing employee information \n" +
-                "4) Remove an existing employee");
+                "2) Retrieve information about existing employees ");
     }
 }
