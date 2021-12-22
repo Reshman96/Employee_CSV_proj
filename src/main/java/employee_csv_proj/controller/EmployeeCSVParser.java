b4ee3c97ea88b333b8c2f2e787b84cfd8002561c
Michael <michael_makam18@hotmail.com>
2021-12-22 14:59:05 +0000

    CSVLogManager added to project.

--- src/main/java/employee_csv_proj/controller/EmployeeCSVParser.java
+++ src/main/java/employee_csv_proj/controller/EmployeeCSVParser.java
@@ -2,11 +2,13 @@ package employee_csv_proj.controller;
 
 import employee_csv_proj.config.Config;
 import employee_csv_proj.controller.Database_management.EmployeeDAO;
+import employee_csv_proj.controller.logging.CSVLogManager;
 import employee_csv_proj.model.Employee;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
 
 //Change line 19 to add the employee to your collection eg. repository.add(new employee(employeeData))
 public class EmployeeCSVParser {
@@ -19,8 +21,10 @@ public class EmployeeCSVParser {
                 String[] employeeData = employeeRecord.split(",");
                 Employee employee = new Employee(employeeData);
                 EmployeeHashMap.addEmployee(employee.getEmpId(), employee);
+                CSVLogManager.getInstance().logMessage(Level.INFO, "Added " + employee.getFirstName() + " " + employee.getLastName() + " to collection.");
             }
         } catch (IOException e) {
+            CSVLogManager.getInstance().logMessage(Level.WARNING, "Couldn't read data from CSV file.");
             e.printStackTrace();
         }
     }
